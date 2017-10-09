5ec5a8b3961c82d2c726e80d2dbb18b6573d8458
faithfem <fmugure@intracitygeeks.org>
2017-10-09 02:22:09 -0400

    java summary

--- aNotes.java
+++ aNotes.java
@@ -2,3 +2,6 @@
 // Statements: if, if/else, and if/else if/else statements are used to conditionally execute blocks of code
 // Ternary Conditional: a shortened version of an if/else statement that returns a value based on the value of a Boolean expression
 // Switch: allows us to check equality of a variable or expression with a value that does not need to be a Boolean
+// ! IS EVALUATED FIRST
+// && IS EVALUATED SECOND
+// || IS EVALUATED THIRD
